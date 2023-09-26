d5657871137542e8cf39795f00b1cc02d1fc3fd8
Johannes Theiner <kontakt@joethei.xyz>
2023-09-26 20:09:46 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -26,6 +26,7 @@ server {
 	listen ${LISTEN};
  	rewrite ^/${REDIRECT_TARGET}(.*)$ $1 last;
 	rewrite ^/(.*)\$ ${REDIRECT_TARGET}\$1 ${REDIRECT_TYPE};
+ 	rewrite ^(.*)/${REDIRECT_TARGET}(.*)$ $1;
 }
 EOF
 
